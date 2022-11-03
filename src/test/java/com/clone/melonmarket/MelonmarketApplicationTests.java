47ae9365842ff15c7d08563a14573302cf3c44b9
Jeongdonghun <arrrrr180@gmail.com>
2022-11-03 14:33:12 +0900

    암호 복호화

--- src/test/java/com/clone/melonmarket/MelonmarketApplicationTests.java
+++ src/test/java/com/clone/melonmarket/MelonmarketApplicationTests.java
@@ -1,5 +1,6 @@
 package com.clone.melonmarket;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +10,27 @@ class MelonmarketApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void jasypt() {
+		String url = "cloneproject4";
+		String username = "AKIAX5IVUWAHM35NZL4B";
+		String password = "ZlZISnq0gmnVmsnXHc7e6p8m0b5qsVwDcOaimw31";
+
+		System.out.println("안뇽");
+		System.out.println(jasyptEncoding(url));
+		System.out.println(jasyptEncoding(username));
+		System.out.println(jasyptEncoding(password));
+	}
+
+	public String jasyptEncoding(String value) {
+
+		String key = "my_jasypt_key";
+		StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+		pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+		pbeEnc.setPassword(key);
+		return pbeEnc.encrypt(value);
+	}
+
 
 }
