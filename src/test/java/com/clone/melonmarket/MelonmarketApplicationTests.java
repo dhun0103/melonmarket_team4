2d704371db7e49da5687f05d39ddad12b37dd21e
Jeongdonghun <arrrrr180@gmail.com>
2022-11-03 17:04:21 +0900

    S3 변경

--- src/test/java/com/clone/melonmarket/MelonmarketApplicationTests.java
+++ src/test/java/com/clone/melonmarket/MelonmarketApplicationTests.java
@@ -13,14 +13,11 @@ class MelonmarketApplicationTests {
 
 	@Test
 	void jasypt() {
-		String url = "cloneproject4";
-		String username = "AKIAX5IVUWAHM35NZL4B";
-		String password = "ZlZISnq0gmnVmsnXHc7e6p8m0b5qsVwDcOaimw31";
+		String url = "imageputbucket";
+
 
 		System.out.println("안뇽");
 		System.out.println(jasyptEncoding(url));
-		System.out.println(jasyptEncoding(username));
-		System.out.println(jasyptEncoding(password));
 	}
 
 	public String jasyptEncoding(String value) {
